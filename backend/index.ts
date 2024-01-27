738bc19fbaee925dc36146d508834b0a047f82b1
Abdullah Abdulkareem <channel.redrex@gmail.com>
2024-01-27 23:41:21 +0300

    "/" route update and vercel backend config edit

--- backend/index.ts
+++ backend/index.ts
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("Welcome to mern stack");
+  return res.send("test");
 });
 
 app.use("/api/users", usersRoute);
